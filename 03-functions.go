30808bd47c6ccf58898a2ec46555758491d91460
leomiotto <54296885+leomiotto@users.noreply.github.com>
2019-09-12 10:16:25 -0300

    Update 03-functions.go

--- 03-functions.go
+++ 03-functions.go
@@ -1,11 +1,27 @@
 package main
 
-import "fmt"
+import "fmt" //Importa o Print
 
-func add (x, y int) int{
+//Soma
+func add (x int, y int) int{
 	return x + y
+}
+
+//Concatena duas Strings - Duas variáveis do mesmo tipo
+func concatena(x, y string) string{
+	return x + y
+}
+
+//Separa dois números - Retorno especificado no enunciado (Naked Return)
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
 }
 
 func main(){
 	fmt.Println("The sum is: ", add(1,2))
+	a := concatena("Olá,", " Marilene!")
+	fmt.Println("A concatenação é:", a)
+	fmt.Println("Números separados: ", split(15))
 }
