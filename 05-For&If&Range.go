4d5ce0fa8ee5a1656c5a5b502f7dda1a03b39ba6
leomiotto <54296885+leomiotto@users.noreply.github.com>
2019-09-13 12:25:12 -0300

    Update and rename 05-For&If.go to 05-For&If&Range.go

--- 05-For&If&Range.go
+++ 05-For&If&Range.go
@@ -14,4 +14,14 @@ func main() {
 	}
 
 	fmt.Println(sum)
+
+	
+	//Range
+	pow := make([]int, 10)
+	for i := range pow {   //Sem usar o value
+		pow[i] = 2 << uint(i)   //O "<<" calcula o dobro de 2, 4, 8... i
+	}
+	for _, value := range pow {   //Sem usar o index
+		fmt.Printf("%d\n", value)
+	}
 }
