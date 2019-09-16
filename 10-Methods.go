f483a528d5b2db17561e8c46e9dc6c0903e2c0c7
leomiotto <54296885+leomiotto@users.noreply.github.com>
2019-09-16 10:30:10 -0300

    Update 10-Methods.go

--- 10-Methods.go
+++ 10-Methods.go
@@ -20,7 +20,7 @@ func main(){
 }
 
 
-//Pointer Receiver - Usado pra modificar o valor do argumento Receiver
+//POINTER RECEIVER - Usado pra modificar o valor do argumento Receiver
 package main
 
 import (
@@ -45,4 +45,57 @@ func main() {
 	v := Vertex{3, 4}
 	v.Scale(10) //Method: O Go interpreta v.Scale(5) como (&v).Scale(5), já que a função deveria receber ponteiro
 	fmt.Println(v.Abs())
+}
+
+
+//INTERFACE
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type geometry interface{
+	area() float64
+	perim() float64
+}
+
+type rect struct{
+	width, height float64
+}
+
+type circle struct{
+	radius float64
+}
+
+//METHODS
+func (r rect) area() float64{   //float64 é o tipo de retorno
+	return r.width * r.height   //retorna a fórmula da área
+}
+func (c circle) area() float64{
+	return c.radius * math.Pi * c.radius
+}
+
+func (r rect) perim() float64{
+	return 2*r.width + 2*r.height
+}
+
+func (c circle) perim() float64{
+	return 2 * math.Pi * c.radius
+}
+
+//INTERFACE
+func measure (g geometry){
+	fmt.Println("Medidas:", g)   //ex: {3,4}
+	fmt.Println("Área: ", g.area())
+	fmt.Println("Perímetro: ", g.perim())
+}
+
+func main(){
+	r1 := rect{width: 3, height: 4}
+	c1 := circle{radius: 5}
+	//rv := &r1   -   Para passar Pointer Receiver
+	measure(r1)
+	measure(c1)
 }
